6b75ef097c65f21490972f15fbdf24c88214de29
杨梦涛 <327750124@qq.com>
2024-08-16 21:02:46 +0800

    跳跃

--- Assets/Scripts/Input/Controls.cs
+++ Assets/Scripts/Input/Controls.cs
@@ -204,6 +204,34 @@ public partial class @Controls: IInputActionCollection2, IDisposable
                     ""isPartOfComposite"": true
                 }
             ]
+        },
+        {
+            ""name"": ""Jumping"",
+            ""id"": ""85857ed0-ee14-49cc-bafc-9baa9a68474f"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""51d2c220-861d-4527-9a58-77f7a6f7de37"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""fdd803a5-0539-43d8-84fd-d647c2e6aec3"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": [
@@ -222,6 +250,9 @@ public partial class @Controls: IInputActionCollection2, IDisposable
         // Movement
         m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
         m_Movement_Move = m_Movement.FindAction("Move", throwIfNotFound: true);
+        // Jumping
+        m_Jumping = asset.FindActionMap("Jumping", throwIfNotFound: true);
+        m_Jumping_Jump = m_Jumping.FindAction("Jump", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -325,6 +356,52 @@ public partial class @Controls: IInputActionCollection2, IDisposable
         }
     }
     public MovementActions @Movement => new MovementActions(this);
+
+    // Jumping
+    private readonly InputActionMap m_Jumping;
+    private List<IJumpingActions> m_JumpingActionsCallbackInterfaces = new List<IJumpingActions>();
+    private readonly InputAction m_Jumping_Jump;
+    public struct JumpingActions
+    {
+        private @Controls m_Wrapper;
+        public JumpingActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Jumping_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Jumping; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(JumpingActions set) { return set.Get(); }
+        public void AddCallbacks(IJumpingActions instance)
+        {
+            if (instance == null || m_Wrapper.m_JumpingActionsCallbackInterfaces.Contains(instance)) return;
+            m_Wrapper.m_JumpingActionsCallbackInterfaces.Add(instance);
+            @Jump.started += instance.OnJump;
+            @Jump.performed += instance.OnJump;
+            @Jump.canceled += instance.OnJump;
+        }
+
+        private void UnregisterCallbacks(IJumpingActions instance)
+        {
+            @Jump.started -= instance.OnJump;
+            @Jump.performed -= instance.OnJump;
+            @Jump.canceled -= instance.OnJump;
+        }
+
+        public void RemoveCallbacks(IJumpingActions instance)
+        {
+            if (m_Wrapper.m_JumpingActionsCallbackInterfaces.Remove(instance))
+                UnregisterCallbacks(instance);
+        }
+
+        public void SetCallbacks(IJumpingActions instance)
+        {
+            foreach (var item in m_Wrapper.m_JumpingActionsCallbackInterfaces)
+                UnregisterCallbacks(item);
+            m_Wrapper.m_JumpingActionsCallbackInterfaces.Clear();
+            AddCallbacks(instance);
+        }
+    }
+    public JumpingActions @Jumping => new JumpingActions(this);
     private int m_KeyboardSchemeIndex = -1;
     public InputControlScheme KeyboardScheme
     {
@@ -346,5 +423,9 @@ public partial class @Controls: IInputActionCollection2, IDisposable
     public interface IMovementActions
     {
         void OnMove(InputAction.CallbackContext context);
+    }
+    public interface IJumpingActions
+    {
+        void OnJump(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,16 +6,34 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D m_Rigidbody;
 
+    [Header("Move")]
     [SerializeField]
     private float m_MoveSpeed = 7.5f;
     private float m_MoveInput;
 
+    [Header("Jump")]
+    [SerializeField]
+    private float m_JumpForce = 5.0f;
+    [SerializeField]
+    private float m_JumpTime = 0.5f;
+    private float m_JumpTimeCounter;
+    private bool m_IsJump = false;
+
+    [Header("Ground")]
+    [SerializeField]
+    private float m_ExtraHeight = 0.25f;
+    [SerializeField]
+    private LayerMask m_Ground;
+
+    [Header("Leg")]
     [SerializeField]
     private GameObject m_LeftLeg;
     [SerializeField]
     private GameObject m_RightLeg;
 
+    private Collider2D m_Collider;
     private Animator m_Animator;
+    private RaycastHit2D m_GroundHit;
 
     private bool m_IsFaceRight;
     public bool IsFaceRight { get => m_IsFaceRight; set => m_IsFaceRight = value; }
@@ -24,6 +42,7 @@ public class Player : MonoBehaviour
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
         m_Animator = GetComponent<Animator>();
+        m_Collider = GetComponent<Collider2D>();
 
         CheckDirectionCheck();
     }
@@ -43,6 +62,34 @@ public class Player : MonoBehaviour
         }
 
         m_Rigidbody.velocity = new Vector2(m_MoveInput * m_MoveSpeed, m_Rigidbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (UserInput.Instance.Controls.Jumping.Jump.WasPerformedThisFrame() && IsGrounded())
+        {
+            m_IsJump = true;
+            m_JumpTimeCounter = m_JumpTime;
+            m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, m_JumpForce);
+        }
+
+        if (UserInput.Instance.Controls.Jumping.Jump.IsPressed())
+        {
+            if (m_JumpTimeCounter > 0 && m_IsJump)
+            {
+                m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, m_JumpForce);
+                m_JumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                m_IsJump = false;
+            }
+        }
+
+        if (UserInput.Instance.Controls.Jumping.Jump.WasReleasedThisFrame())
+        {
+            m_IsJump = false;
+        }
     }
 
     private void CheckDirectionCheck()
@@ -72,5 +119,16 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Move();
+        Jump();
+    }
+
+    private bool IsGrounded()
+    {
+        m_GroundHit = Physics2D.BoxCast(m_Collider.bounds.center, m_Collider.bounds.size, 0f, Vector2.down, m_ExtraHeight, m_Ground);
+        if (m_GroundHit.collider != null)
+        {
+            return true;
+        }
+        return false;
     }
 }
